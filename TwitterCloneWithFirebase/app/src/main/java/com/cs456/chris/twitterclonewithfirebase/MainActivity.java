7d8c58b682edb950cc1480b778abc3ca0b121dad
Chris Prestwich <capchu@gmail.com>
2016-01-28 16:13:56 -0700

    start of twitter

---
+++ TwitterCloneWithFirebase/app/src/main/java/com/cs456/chris/twitterclonewithfirebase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cs456.chris.twitterclonewithfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
